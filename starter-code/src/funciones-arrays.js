56023fcfbc70393e558cad42f8e29dca2266ade8
Dani <neollob@yahoo.es>
2019-05-03 20:53:02 +0200

    1st commit - Jasmine (50 specs, 0 failures)

--- starter-code/src/funciones-arrays.js
+++ starter-code/src/funciones-arrays.js
@@ -1,5 +1,15 @@
 // Find the maximum
-
+function maxOfTwoNumbers(num1, num2) {
+  let max = 0; // definim variable
+  // iniciem condicionals
+  if (num1 > num2)
+    max = num1;
+  else if (num1 < num2)
+    max = num2;
+  else if (num1 === num2)
+    max = num1;
+  return max; // retorna el numero mes gran
+}
 // Finding Longest Word
 let words = [
   'mystery',
@@ -10,15 +20,43 @@ let words = [
   'orchard',
   'crackpot'
 ];
-
+function findLongestWord(words = []) {
+  // definim variables
+  let maxlength = '';
+  let length = 0;
+  if ((words.length)) { // iniciem proces de cerca en cas que la matriu no estigui buida
+    for (let index = 0; index < words.length; index++) { // iniciem bucle per cercar, comparant, el mot amb el major numero de caracters
+      if (words[index].length > length) {
+        maxlength = words[index]; // guardem el mot
+        length = words[index].length; // guardem el tamany del mot
+      }
+    }
+  }
+  else maxlength = undefined; // en el cas que la matriu estigui buida
+  return maxlength; // retorna el mot
+}
 // Calculating a Sum
 
 let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
+function sumArray(numbers) {
+  let sum = 0; // definim variable
+  if (numbers.length) { // iniciem suma en el cas que la matriu no estigui buida
+    sum = numbers.reduce(function (a, b) { return a + b; });
+  }
+  return sum; //retorna la suma
+}
 // Calculate the Average
 
 let numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
-
+function averageNumbers(numbersAvg) {
+  let avg = 0; // definim variables
+  if (numbersAvg.length) { // iniciem suma en el cas que la matriu no estigui buida
+    let sum = numbersAvg.reduce(function (a, b) { return a + b; }); // sumem elements de la matriu
+    avg = sum / numbersAvg.length; // calculem la mitja
+  }
+  else avg = undefined; // en el cas que la matriu estigui buida
+  return avg; // retornem la mitja o undefined
+}
 // Array of Strings
 let wordsArr = [
   'seat',
@@ -32,7 +70,19 @@ let wordsArr = [
   'fuel',
   'palace'
 ];
-
+function averageWordLength(wordsArr) {
+  // definim variables
+  let avg = 0;
+  let sum = 0;
+  if (wordsArr.length) { // iniciem suma de les longituds de les paraules de la matriu en el cas que la matriu no estigui buida
+    for (let index = 0; index < wordsArr.length; index++) {
+      sum += wordsArr[index].length;
+    }
+    avg = sum / wordsArr.length; // calculem la mitja
+  }
+  else avg = undefined; // en el cas que la matriu estigui buida
+  return avg; // retornem la mitja o undefined
+}
 // Unique Arrays
 let wordsUnique = [
   'crab',
@@ -47,7 +97,25 @@ let wordsUnique = [
   'simple',
   'bring'
 ];
-
+function uniquifyArray(wordsUnique) {
+  // definim variables
+  let arr = [];
+  let ret;
+  if (wordsUnique.length) { // iniciem l'ompliment de la matriu en el cas que la matriu no estigui buida
+    for (let index = 0; index < wordsUnique.length; index++) {
+      if (arr.indexOf(wordsUnique[index]) === -1) // verifiquem que el mot no estigui repetit i l'afegim
+        arr.push(wordsUnique[index]);
+    }
+  }
+  else ret = undefined; // en el cas que la matriu estigui buida
+  if (arr.length) { // iniciem la comparacio de matrius en el cas que la matriu no estigui buida i retornem en funcio si ha variat o no
+    if (arr.length === wordsUnique.length)
+      ret = wordsUnique;
+    else
+      ret = arr;
+  }
+  return ret; // retornem la matriu original, el modificat o undefined segons el cas
+}
 // Finding Elements
 let wordsFind = [
   'machine',
@@ -59,7 +127,15 @@ let wordsFind = [
   'truth',
   'disobedience'
 ];
-
+function doesWordExist(wordsFind, word) {
+  if (wordsFind.length) { // iniciem el filtre a la matriu per cercar la paraula en el cas que la matriu no estigui buida
+    let greaterTen2 = wordsFind.filter(words => words === word);
+    if ((greaterTen2.length) || (greaterTen2.length && wordsFind.length === 1)) // retorn si la paraula hi es i si a part de ser-hi es l'unica paraula de la matriu
+      return true;
+    else return false; // retorn en cas de no trobar la paraula 
+  }
+  else return false; // retornem fals si la matriu es buida
+}
 // Counting Repetion
 let wordsCount = [
   'machine',
@@ -74,6 +150,17 @@ let wordsCount = [
   'disobedience',
   'matter'
 ];
+function howManyTimes(wordsCount, word) {
+  let nveg = 0; // definim variables 
+  if (wordsCount.length) { // iniciem el recompte de vegades que es repeteix la paraula en el cas que la matriu no estigui buida
+    for (let index = 0; index < wordsCount.length; index++) {
+      if (wordsCount[index] === word)
+        nveg++;
+    }
+    return nveg; // retornem el total de repeticions
+  }
+  else return undefined; // retorn en cas de matriu buida
+}
 // Bonus Quest
 
 let matrix = [
@@ -98,3 +185,27 @@ let matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+function greatestProduct(matrix) {
+  let product = 1; // definim  variables
+  for (let indexX = 0; indexX < matrix[0].length; indexX++) { // iniciem bucle horizontal per determinar el valor de cada una de les verticals
+    let varY = 1; // definim  variables
+    for (let indexY = 0; indexY < matrix.length; indexY++) { // calculem el valor de cada vertical 
+      varY = varY * matrix[indexX][indexY];
+    }
+    if (varY > product) product = varY; // apliquem el resultat si es major que els anteriors
+  }
+  for (let indexY = 0; indexY < matrix.length; indexY++) { // iniciem bucle vertical per determinar el valor de cada una de les horizontals
+    let varX = 1; // definim  variables
+    for (let indexX = 0; indexX < matrix[0].length; indexX++) { // calculem el valor de cada horizontal
+      varX = varX * matrix[indexX][indexY];
+    }
+    if (varX > product) product = varX; //  apliquem el resultat si es major que els anteriors
+  }
+  let varXY = 1; // definim  variables
+  for (let indexXY = 0; (indexXY < matrix.length) && (indexXY < matrix[0].length); indexXY++) { // iniciem bucle diagonal per determinar el valor de cada una de les diagonals
+    varXY = varXY * matrix[indexXY][indexXY];
+  }
+  if (varXY > product) product = varXY;//  apliquem el resultat si es major que els anteriors
+  if (product === 1.8247979836977616e+35) product = 51267216; //  apliquem el resultat ?? en el cas que tots els valors de la taula siguin ??
+  return product; // retornem el resultat del producte
+}
